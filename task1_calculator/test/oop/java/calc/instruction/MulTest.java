825c521dac92db44d4a17b1c8fea21cd3eafdd80
Виктория <55043240+zzomg@users.noreply.github.com>
2020-03-02 01:00:30 +0700

    Create MulTest.java

---
+++ task1_calculator/test/oop/java/calc/instruction/MulTest.java
@@ -0,0 +1,38 @@
+package oop.java.calc.instruction;
+
+import oop.java.calc.InstrFactory;
+import oop.java.calc.exception.CalcException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MulTest {
+
+    @Test
+    void execute() {
+        List<Double> stack = new ArrayList<>();
+        List<String> args = new ArrayList<>();
+
+        stack.add((double) 5);
+        stack.add((double) 10);
+
+        InstrFactory.getInstance().create("oop.java.calc.instruction.Mul").execute(stack, args);
+
+        Assertions.assertEquals(50, stack.get(0));
+
+        args.add("5");
+
+        Assertions.assertThrows(CalcException.class,
+                () -> InstrFactory.getInstance().create("oop.java.calc.instruction.Mul").execute(stack, args));
+
+        stack.clear();
+        args.clear();
+
+        Assertions.assertThrows(CalcException.class,
+                () -> InstrFactory.getInstance().create("oop.java.calc.instruction.Mul").execute(stack, args));
+    }
+}
