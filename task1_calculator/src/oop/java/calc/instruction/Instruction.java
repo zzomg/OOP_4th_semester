2c3079a19502052bda30477079835084ddf635f8
Виктория <55043240+zzomg@users.noreply.github.com>
2020-03-02 10:33:45 +0700

    Update Instruction.java

--- task1_calculator/src/oop/java/calc/instruction/Instruction.java
+++ task1_calculator/src/oop/java/calc/instruction/Instruction.java
@@ -4,4 +4,5 @@ import java.util.List;
 
 public interface Instruction {
     void execute(List<Double> stack, List<String> args);
+    //TODO: void execute(List<Double> stack, List<String> args, Map<String, Double> vars);
 }
