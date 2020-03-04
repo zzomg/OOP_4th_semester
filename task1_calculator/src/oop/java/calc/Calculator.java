e3d05436a5fda167fa7918d1a3a731b9e91cd412
Виктория <55043240+zzomg@users.noreply.github.com>
2020-03-04 20:46:03 +0700

    Add files via upload

--- task1_calculator/src/oop/java/calc/Calculator.java
+++ task1_calculator/src/oop/java/calc/Calculator.java
@@ -12,19 +12,7 @@ public class Calculator
     public static final Logger LOGGER = Logger.getLogger(Calculator.class.getName());
 
     public List<Double> stack = new ArrayList<>();
-
-    public static final String defFilePath = "src/defined.properties";
-    
-    //TODO: убрать обращение к файлам
-    public static void flushDefFile(String defFileName)
-    {
-        try {
-            RandomAccessFile defFile = new RandomAccessFile(defFileName, "rw");
-            defFile.setLength(0L);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    public Map<String, Double> vars = new TreeMap<>();
 
     public double calculate(String inputSourceName) throws FileNotFoundException
     {
@@ -50,63 +38,24 @@ public class Calculator
                         instrName = tokenizer.nextToken().toLowerCase();
                     }
                     if(tokenizer.countTokens() > 0) {
-                        args.add(checkValueDefined(tokenizer.nextToken(), instrName));
+                        args.add(tokenizer.nextToken());
                     }
                 }
                 if (!instrName.startsWith("#") && countTokens > 0) {
-                    InstrFactory.getInstance().create(getInstrClass(instrName)).execute(stack, args);
+                    InstrFactory.getInstance().create(instrName).execute(stack, args, vars);
                 }
                 args.clear();
             }
         }
         if (stack.size() > 1) {
-            flushDefFile(defFilePath);
-            LOGGER.log(Level.SEVERE, "Unexpected exception");
+            LOGGER.log(Level.SEVERE, "Unexpected exception: Result can not be obtained");
             throw new CalcException("Result can not be obtained: Stack contains more than 1 element.");
         } else if (stack.size() == 0) {
             LOGGER.log(Level.WARNING, "FINISH: Result can not be obtained: Stack is empty.");
         } else {
             result = stack.get(0);
         }
-        flushDefFile(defFilePath);
         LOGGER.log(Level.INFO, "FINISH: Result is {0}", result);
         return result;
-    }
-
-    private String getInstrClass(String instrName)
-    {
-        String instrClass = "";
-        try(InputStream input = Calculator.class.getClassLoader().getResourceAsStream("config.properties")) {
-            Properties prop = new Properties();
-            assert input != null;
-            prop.load(input);
-            instrClass = prop.getProperty(instrName, instrName);
-        } catch (IOException e) {
-            flushDefFile(defFilePath);
-            e.printStackTrace();
-        }
-        return instrClass;
-    }
-
-    private String checkValueDefined(String token, String instrName)
-    {
-        String def = "";
-        try(InputStream input = new FileInputStream("src/defined.properties")) {
-            Properties prop = new Properties();
-            prop.load(input);
-            if (instrName.equals("define")) {
-                for(Object key : prop.keySet()) {
-                    if(token.equals(key.toString())) {
-                        flushDefFile(defFilePath);
-                        throw new CalcException("Error: Value was already defined");
-                    }
-                }
-            }
-            def = prop.getProperty(token, token);
-        } catch (IOException e) {
-            flushDefFile(defFilePath);
-            e.printStackTrace();
-        }
-        return def;
     }
 }
--- task1_calculator/src/oop/java/calc/InstrFactory.java
+++ task1_calculator/src/oop/java/calc/InstrFactory.java
@@ -1,9 +1,14 @@
 package oop.java.calc;
 
+import oop.java.calc.exception.CalcException;
 import oop.java.calc.instruction.Instruction;
-import oop.java.calc.instruction.Sum;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+import java.util.Properties;
+import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,8 +16,9 @@ public class InstrFactory extends AbstractFactory
 {
     public static final Logger LOGGER = Logger.getLogger(InstrFactory.class.getName());
 
+    private Map<String, Class<?>> classMap = new TreeMap<>();
+
     private static volatile InstrFactory instance;
-    private Map<String, Class> classMap = new TreeMap<>();
 
     public static InstrFactory getInstance() {
         InstrFactory localInstance = instance;
@@ -27,23 +33,35 @@ public class InstrFactory extends AbstractFactory
         return localInstance;
     }
     
-     private InstrFactory() {
-        //TODO: здесь парсим конфиг и заливаем в мапу команды. в create только создаем экземпляры
-
+     private InstrFactory()
+     {
+         try(InputStream input = InstrFactory.class.getClassLoader().getResourceAsStream("config.properties")) {
+             Properties properties = new Properties();
+             assert input != null;
+             properties.load(input);
+             for(Object prop : properties.keySet()) {
+                 classMap.put(prop.toString(), Class.forName(properties.getProperty(prop.toString())));
+             }
+         } catch (IOException e) {
+             LOGGER.log(Level.SEVERE, "Unexpected exception: Config file was not found");
+             throw new CalcException("Error: Config file was not found");
+         } catch (ClassNotFoundException e) {
+             LOGGER.log(Level.SEVERE, "Unexpected exception: Instruction was not found");
+             throw new CalcException("Error: Instruction was not found");
+         }
     }
 
     @Override
     public Instruction create(String key) {
-        Class<?> instrCls;
         Object instr = new Object();
         try {
-            instrCls = Class.forName(key);
-            instr = instrCls.getDeclaredConstructor().newInstance();
-        } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException |
+            if(classMap.containsKey(key)) {
+                instr = classMap.get(key).getDeclaredConstructor().newInstance();
+            }
+        } catch (InstantiationException | NoSuchMethodException |
                 IllegalAccessException | InvocationTargetException e) {
-            LOGGER.log(Level.SEVERE, "Unexpected exception");
-            e.printStackTrace();
-            //TODO: бросить свои эксепшены
+            LOGGER.log(Level.SEVERE, "Unexpected exception : Couldn't create instruction");
+            throw new CalcException("Error: Couldn't create instruction");
         }
         assert instr instanceof Instruction;
         return (Instruction) instr;
