9927d67d523f32b373437669a8dc7181df694f9a
Виктория <55043240+zzomg@users.noreply.github.com>
2020-04-19 12:44:32 +0300

    Update Main.java

--- task3_factory/Main.java
+++ task3_factory/Main.java
@@ -6,6 +6,18 @@ public class Main {
         factory.initialize(10, 10, 30, 10,
                 1000, 1500, 500, 5000,
                 3, 6, 3);
+        /*engineStorageCapacity = 10
+        * carcassStorageCapacity = 10
+        * accessoriesStorageCapacity = 30
+        * carStorageCapacity = 10
+        * engineSupCooldown = 1000
+        * carcassSupCooldown = 1500
+        * accessoriesSupCooldown = 500
+        * dealerCooldown = 5000
+        * n_accessoriesSuppliers = 3
+        * n_workers = 6
+        * n_dealers = 3
+        * */
         factory.start();
     }
 }
